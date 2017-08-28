f61980a356e8293281ca785194a48f998e9383b8
xx254 <xx254@cornell.edu>
2017-08-28 18:05:42 -0400

    MovieFinder v1

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  //template: '<navbar></navbar>'
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent { }
---
+++ app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'jumbotron',
+  templateUrl: 'jumbotron.component.html'
+})
+
+export class JumbotronComponent {
+    private jbtHeading:string;
+    private jbtText:string;
+    private jbtBtnText:string;
+    private jbtBtnUrl:string;
+
+    constructor(){
+        this.jbtHeading = "Hello World";
+        this.jbtText = "This is a simple hero unit, a simple jumbotron ... text";
+        this.jbtBtnText = "Read More";
+        this.jbtBtnUrl = "/about"
+    }
+}
